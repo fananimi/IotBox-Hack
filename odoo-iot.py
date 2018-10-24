238781f251235690dba67b6ace77d6ccd557476b
Fanani M. Ihsan <fanani.mi@gmail.com>
2018-10-24 11:15:29 +0700

    [IMP] handling keybord interrupt

--- odoo-iot.py
+++ odoo-iot.py
@@ -1,10 +1,12 @@
 import sys
+import signal
 
 from odoo.ui import Main
 from PyQt4.QtGui import QApplication
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QApplication(sys.argv)
     window = Main()
     window.show()
