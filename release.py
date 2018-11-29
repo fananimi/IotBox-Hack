da6c6761cf8dc5995b11860e0f659870395341cc
Fanani M. Ihsan <fanani.mi@gmail.com>
2018-11-29 14:44:55 +0700

    [RELEASE] release version 1.0

--- release.py
+++ release.py
@@ -9,7 +9,7 @@ RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA,
 
 # version_info format: (MAJOR, MINOR, MICRO, OS, ARCH)
 os = 'Mac' if platform.system().lower() == 'darwin' else platform.system()
-version_info = (1, 0, 0, ALPHA, os)
+version_info = (1, 0, 0, FINAL, os)
 version = '.'.join(str(s) for s in version_info[:2]) + RELEASE_LEVELS_DISPLAY[version_info[3]] + " " + "(" + str(version_info[4]) + ")"
 series = serie = major_version = '.'.join(str(s) for s in version_info[:2])
 
