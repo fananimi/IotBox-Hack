979bd10a9e80c16a5c9901d8ac76b760df376be3
Fanani M. Ihsan <fanani.mi@gmail.com>
2020-03-04 20:51:54 +0700

    remove unused code

--- addons/hw_escpos/escpos/printer.py
+++ addons/hw_escpos/escpos/printer.py
@@ -3,8 +3,6 @@
 import os
 import usb.core
 import usb.util
-import serial
-import socket
 
 from escpos import *
 from constants import *
@@ -161,67 +159,3 @@ class Usb(Escpos):
         if self.device:
             self.close()
         self.device = None
-
-
-class Serial(Escpos):
-    """ Define Serial printer """
-
-    def __init__(self, devfile="/dev/ttyS0", baudrate=9600, bytesize=8, timeout=1):
-        """
-        @param devfile  : Device file under dev filesystem
-        @param baudrate : Baud rate for serial transmission
-        @param bytesize : Serial buffer size
-        @param timeout  : Read/Write timeout
-        """
-        self.devfile  = devfile
-        self.baudrate = baudrate
-        self.bytesize = bytesize
-        self.timeout  = timeout
-        self.open()
-
-    def open(self):
-        """ Setup serial port and set is as escpos device """
-        self.device = serial.Serial(port=self.devfile, baudrate=self.baudrate, bytesize=self.bytesize, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=self.timeout, dsrdtr=True)
-
-        if self.device is not None:
-            print "Serial printer enabled"
-        else:
-            print "Unable to open serial printer on: %s" % self.devfile
-
-    def _raw(self, msg):
-        """ Print any command sent in raw format """
-        self.device.write(msg)
-
-    def __del__(self):
-        """ Close Serial interface """
-        if self.device is not None:
-            self.device.close()
-
-
-class Network(Escpos):
-    """ Define Network printer """
-
-    def __init__(self,host,port=9100):
-        """
-        @param host : Printer's hostname or IP address
-        @param port : Port to write to
-        """
-        self.host = host
-        self.port = port
-        self.open()
-
-    def open(self):
-        """ Open TCP socket and set it as escpos device """
-        self.device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.device.connect((self.host, self.port))
-
-        if self.device is None:
-            print "Could not open socket for %s" % self.host
-
-    def _raw(self, msg):
-        self.device.send(msg)
-
-    def __del__(self):
-        """ Close TCP connection """
-        self.device.close()
-
