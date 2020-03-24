6989f5fbd0a11feb9ce2b133a759cbf02b182471
Fanani M. Ihsan <fanani.mi@gmail.com>
2020-03-24 16:10:41 +0700

    register zpl in addons

--- addons/__init__.py
+++ addons/__init__.py
@@ -1,4 +1,5 @@
-from . import hw_escpos
 from . import hw_proxy
+from . import hw_escpos
+from . import hw_zpl
 
-__all__ = ['hw_escpos', 'hw_proxy']
+__all__ = ['hw_escpos', 'hw_proxy', 'hw_zpl']
--- addons/hw_zpl/zpl/zpl.py
+++ addons/hw_zpl/zpl/zpl.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-import six
-from abc import ABCMeta, abstractmethod
+from zpl import Label
 
 
-@six.add_metaclass(ABCMeta)
-class Zpl:
+class Zpl(Label):
     """ ZPL Printer object
 
     This class is the abstract base class for an esc/pos-printer. The printer implementations are children of this
